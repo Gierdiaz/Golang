404b112c96dc588e9eb5d8b0b3b255df7a51bcf9
gierdiaz <gierdiaz@hotmail.com>
2022-08-31 22:42:14 -0300

    Go

--- 25 - Crud/main.go
+++ 25 - Crud/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"crud/servidor"
 
 	"github.com/gorilla/mux"
 )
@@ -11,7 +12,15 @@ import (
 func main() {
 	//CRUD = CREATE, READ, UPDATE, DELETE
 
+	//CREATE - POST
+	//READ - GET
+	//UPDATE - PUT
+	//DELETE - DELETE
+
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+
+
 
 	fmt.Println("Escutando na porta 4000")
 	log.Fatal(http.ListenAndServe(":4000", router))
